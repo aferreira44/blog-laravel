22732d09d01ae06c67d4f08509f41bb5481f773e
André Ferreira <contato@andreferreira.me>
2017-10-24 02:26:25 -0200

    Bootstrap Blog Layout Example

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,12 +7,12 @@
     <meta name="author" content="">
     <link rel="icon" href="../../../../favicon.ico">
 
-    <title>Album example for Bootstrap</title>
+    <title>The Bootstrap Blog</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
-    <link href="/css/album.css" rel="stylesheet">
+    <link href="/css/blog.css" rel="stylesheet">
   </head>
 
   <body>

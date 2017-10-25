83a30e819ee355ff9110ad3eae0e48d255fed96d
André Ferreira <contato@andreferreira.me>
2017-10-25 10:14:12 -0200

    Update PostFactory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,10 +1,13 @@
 <?php
 
 use Faker\Generator as Faker;
+use Carbon\Carbon;
 
 $factory->define(App\Post::class, function (Faker $faker) {
     return [
         'title' => $faker->text(15),
-        'body' => $faker->text(140)
+        'body' => $faker->text(1000),
+        'created_at' => $faker->date,
+        'updated_at' => $faker->date
     ];
 });
